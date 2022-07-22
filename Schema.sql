26e611ebf980619f9b3a4dba025b982bd164f6d9
Honest <TMAM9@unigroupinc.com>
2022-07-22 16:12:46 -0500

    Working Code

--- Schema.sql
+++ Schema.sql
@@ -98,3 +98,51 @@ AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31')
 ;
 SELECT * FROM retirement_info
 ;
+DROP TABLE retirement_info
+;
+-- Create new table for retiring employees
+SELECT emp_no, first_name, last_name
+INTO retirement_info
+FROM employees
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+AND (hire_date BETWEEN '1985-01-01' AND '1988-12-31');
+-- Check the table
+SELECT * FROM retirement_info;
+SELECT e.emp_no, e.first_name, e.last_name,
+ti.title, ti.from_date, ti.to_date
+INTO retirement_titles
+FROM employees AS e
+INNER JOIN titles AS ti
+on e.emp_no = ti.emp_no
+WHERE (birth_date BETWEEN '1952-01-01' AND '1955-12-31')
+ORDER BY e.emp_no
+;
+-- Use Dictinct with Orderby to remove duplicate rows
+SELECT DISTINCT ON (rt.emp_no)rt.emp_no,
+rt.first_name,
+rt.last_name, rt.title
+INTO unique_titles
+FROM retirement_titles as rt
+WHERE to_date = ('9999-01-01')
+ORDER BY rt.emp_no ASC, to_date DESC
+;
+-- Number of employees about to retire
+SELECT COUNT(ut.title), ut.title
+INTO retiring_titles
+FROM unique_titles AS ut
+GROUP BY ut.title
+ORDER BY (ut.count) DESC
+;
+-- Mentorship Eligibility Table
+SELECT DISTINCT ON(e.emp_no)e.emp_no, e.first_name, 
+e.last_name, e.birth_date, de.from_date, de.to_date,ti.title
+INTO mentorship_eligibility
+FROM employees AS e
+INNER JOIN dept_emp AS de
+ON (e.emp_no = de.emp_no)
+INNER JOIN titles AS ti
+ON (e.emp_no = ti.emp_no)
+WHERE (e.birth_date BETWEEN '1965-01-01' AND '1965-12-31')
+AND de.to_date = ('9999-01-01')
+ORDER BY emp_no
+;
